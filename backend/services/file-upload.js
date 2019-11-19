e778be0727b858ceb3a11406bb273916fab00c25
v-pravotorov <jtone028@gmail.com>
2019-11-19 11:19:22 +0300

    v.1.0

--- backend/services/file-upload.js
+++ backend/services/file-upload.js
@@ -10,7 +10,8 @@ aws.config.update({
     region: 'ca-central-1'
 })
 
-const s3 = new aws.S3()
+const s3 = new aws.S3();
+const FILE_PERMISSION = "public-read-write";
  
 
 const fileFilter = (req, file, cb) => {
@@ -22,11 +23,11 @@ const fileFilter = (req, file, cb) => {
 }
 
 const upload = multer({
-  fileFilter,
-  storage: multerS3({ 
+  fileFilter, 
+  storage: multerS3({
+    acl: FILE_PERMISSION, 
     s3: s3,
-    bucket: 'penguins-image',
-    ACL: 'public-read-write',  
+    bucket: 'penguins-image', 
     metadata: function (req, file, cb) {
       cb(null, {fieldName: "Testing_Meta_data"});
     },
